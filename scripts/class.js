0c8656a4bf175b2d9ec48ebfe2cdc64c72a48ef0
jessinmacdon <jessinmacdon@gmail.com>
2023-09-19 20:05:06 +0200

    implementation of the select. creation of the new class.html file, combining js code for class management into 1 file

--- scripts/class.js
+++ scripts/class.js
@@ -1,5 +1,16 @@
+// Define getNextClassInitial function globally
+function getNextClassInitial(existingClassInitials) {
+	let nextInitial = "A";
+	while (existingClassInitials.includes(nextInitial)) {
+		nextInitial = String.fromCharCode(nextInitial.charCodeAt(0) + 1);
+	}
+	return nextInitial;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
 	const classTables = document.querySelector(".class-tables");
+	// Defining initialsSelect
+	const initialsSelect = document.getElementById("newClassInitials");
 
 	try {
 		const classResponse = await fetch("mockData/class.json");
@@ -18,6 +29,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 		classNames.forEach((className) => {
 			className.addEventListener("click", async () => {
 				const classIndex = parseInt(className.getAttribute("data-class-index"));
+				// Replace this with the actual data.
+				// The idea is to fetch the list of students assigned to that class, which is then used to populate the ClassList.
+				// Make sure to pull data from a document or table containing the students' names and class.
 				const classListResponse = await fetch("mockData/form1a.json");
 				const classListData = await classListResponse.json();
 
@@ -30,9 +44,9 @@ document.addEventListener("DOMContentLoaded", async () => {
 				classData.forEach((student, index) => {
 					const row = document.createElement("tr");
 					row.innerHTML = `
-						<td>${index + 1}</td>
-						<td>${student.first_name} ${student.last_name}</td>
-					`;
+            <td>${index + 1}</td>
+            <td>${student.first_name} ${student.last_name}</td>
+          `;
 
 					if (
 						student.first_name === classListData.class_master.first_name &&
@@ -73,7 +87,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	// Hide the class initials input initially
 	initialsInput.style.display = "none";
 
-	// Load JSON data
+	// Load JSON data for class initials
 	fetch("mockData/class.json")
 		.then((response) => response.json())
 		.then((data) => {
@@ -98,7 +112,13 @@ document.addEventListener("DOMContentLoaded", async () => {
 				stageSelect.innerHTML = "";
 				classExistsMessage.style.display = "none";
 				classCreatedMessage.style.display = "none";
-				initialsInput.value = "";
+
+				// Get existing class initials
+				const existingClassInitials =
+					selectedBranchData.sections[0].stages[0].classes.map(
+						(cls) => cls.name
+					);
+				const nextClassInitial = getNextClassInitial(existingClassInitials);
 
 				if (selectedBranchData) {
 					selectedBranchData.sections[0].stages.forEach((stage) => {
@@ -109,7 +129,23 @@ document.addEventListener("DOMContentLoaded", async () => {
 					});
 
 					stageSelect.style.display = "block";
-					initialsInput.style.display = "block"; // Display class initials input
+					initialsSelect.style.display = "block"; // Display class initials select
+
+					// Populate class initials select
+					initialsSelect.innerHTML = "";
+					for (const initial of existingClassInitials) {
+						const option = document.createElement("option");
+						option.value = initial;
+						option.textContent = initial;
+						option.disabled = true; // Disable existing options
+						initialsSelect.appendChild(option);
+					}
+
+					// Add the next available class initial
+					const nextInitialOption = document.createElement("option");
+					nextInitialOption.value = nextClassInitial;
+					nextInitialOption.textContent = nextClassInitial;
+					initialsSelect.appendChild(nextInitialOption);
 				}
 			});
 
@@ -117,7 +153,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 			createClassBtn.addEventListener("click", () => {
 				const selectedBranch = branchSelect.value;
 				const selectedStage = stageSelect.value;
-				const classInitials = initialsInput.value.toUpperCase();
+				const classInitials = initialsSelect.value.toUpperCase();
 
 				const selectedBranchData = branches.find(
 					(branch) => branch.name === selectedBranch
@@ -157,58 +193,112 @@ document.addEventListener("DOMContentLoaded", async () => {
 		.catch((error) => {
 			console.error("Error loading JSON data:", error);
 		});
+
+	//create and display the class structure for the entire school
+	function createTable(branchName, sectionName, stages) {
+		const table = document.createElement("table");
+		table.className = "class-table";
+
+		// Create table header
+		const thead = document.createElement("thead");
+		const headerRow = document.createElement("tr");
+		const headerBranch = document.createElement("th");
+		headerBranch.textContent = "Branch";
+		const headerSection = document.createElement("th");
+		headerSection.textContent = "Section";
+
+		headerRow.appendChild(headerBranch);
+		headerRow.appendChild(headerSection);
+
+		for (const stage of stages) {
+			const headerStage = document.createElement("th");
+			headerStage.textContent = stage.name;
+			headerRow.appendChild(headerStage);
+		}
+
+		thead.appendChild(headerRow);
+		table.appendChild(thead);
+
+		// Create table body
+		const tbody = document.createElement("tbody");
+		const bodyRow = document.createElement("tr");
+		const bodyBranch = document.createElement("td");
+		bodyBranch.textContent = branchName;
+		const bodySection = document.createElement("td");
+		bodySection.textContent = sectionName;
+
+		bodyRow.appendChild(bodyBranch);
+		bodyRow.appendChild(bodySection);
+
+		for (const stage of stages) {
+			const bodyStage = document.createElement("td");
+			const classButtonsWrapper = document.createElement("div"); // Create a div to hold the class buttons
+			classButtonsWrapper.className = "btn-group btn-group-toggle";
+			classButtonsWrapper.setAttribute("data-toggle", "buttons");
+
+			const classNames = stage.classes.map((cls) => cls.name);
+
+			classNames.forEach((className, index) => {
+				const classButton = document.createElement("button");
+				classButton.className = "btn class-btn";
+				classButton.textContent = className; // Display class name
+
+				// Add a click event listener to each class button
+				classButton.addEventListener("click", async () => {
+					const classIndex = index; // Use the index to identify the class
+					// Fetch and display the class list for the clicked class
+					const classListResponse = await fetch("mockData/form1a.json");
+					const classListData = await classListResponse.json();
+
+					const classListModal = document.querySelector(".modal-classlist");
+					const modalContent =
+						classListModal.querySelector(".modal-table-body");
+					modalContent.innerHTML = "";
+
+					// Populate modal with class list content
+					const classData = classListData.students;
+					classData.forEach((student, index) => {
+						const row = document.createElement("tr");
+						row.innerHTML = `
+          <td>${index + 1}</td>
+          <td>${student.first_name} ${student.last_name}</td>
+        `;
+
+						if (
+							student.first_name === classListData.class_master.first_name &&
+							student.last_name === classListData.class_master.last_name
+						) {
+							row.querySelector("td:last-child").classList.add("class-master");
+						} else if (
+							student.first_name === classListData.head_boy.first_name &&
+							student.last_name === classListData.head_boy.last_name
+						) {
+							row.querySelector("td:last-child").classList.add("head-boy");
+						} else if (
+							student.first_name === classListData.head_girl.first_name &&
+							student.last_name === classListData.head_girl.last_name
+						) {
+							row.querySelector("td:last-child").classList.add("head-girl");
+						}
+
+						modalContent.appendChild(row);
+					});
+
+					// Show the modal
+					const modal = new bootstrap.Modal(classListModal);
+					modal.show();
+				});
+
+				classButtonsWrapper.appendChild(classButton);
+			});
+
+			bodyStage.appendChild(classButtonsWrapper);
+			bodyRow.appendChild(bodyStage);
+		}
+
+		tbody.appendChild(bodyRow);
+		table.appendChild(tbody);
+
+		return table;
+	}
 });
-
-//create and display the class structure for the entire school
-function createTable(branchName, sectionName, stages) {
-	const table = document.createElement("table");
-	table.className = "class-table";
-
-	// Create table header
-	const thead = document.createElement("thead");
-	const headerRow = document.createElement("tr");
-	const headerBranch = document.createElement("th");
-	headerBranch.textContent = "Branch";
-	const headerSection = document.createElement("th");
-	headerSection.textContent = "Section";
-
-	headerRow.appendChild(headerBranch);
-	headerRow.appendChild(headerSection);
-
-	for (const stage of stages) {
-		const headerStage = document.createElement("th");
-		headerStage.textContent = stage.name;
-		headerRow.appendChild(headerStage);
-	}
-
-	thead.appendChild(headerRow);
-	table.appendChild(thead);
-
-	// Create table body
-	const tbody = document.createElement("tbody");
-	const bodyRow = document.createElement("tr");
-	const bodyBranch = document.createElement("td");
-	bodyBranch.textContent = branchName;
-	const bodySection = document.createElement("td");
-	bodySection.textContent = sectionName;
-
-	bodyRow.appendChild(bodyBranch);
-	bodyRow.appendChild(bodySection);
-
-	for (const stage of stages) {
-		const bodyStage = document.createElement("td");
-		const classNames = stage.classes.map((cls) => cls.name).join(", ");
-
-		// Add class-name class to the cell for styling and click event
-		bodyStage.classList.add("class-name");
-		bodyStage.setAttribute("data-class-index", stage.classes[0].index); // Storing the class index
-		bodyStage.textContent = classNames;
-
-		bodyRow.appendChild(bodyStage);
-	}
-
-	tbody.appendChild(bodyRow);
-	table.appendChild(tbody);
-
-	return table;
-}
